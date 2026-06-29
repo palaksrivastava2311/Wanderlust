c78b156e45a47f7e248818aa5a3b573a5e89d071
palaksrivastava2311 <palaksri8135@gmail.com>
2026-06-30 03:26:56 +0530

    Initial Wanderlust project

---
+++ models/listing.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title : {
+        type: String,
+        required : true
+    } ,
+    description : String,
+    image: {
+        filename: {
+            type: String,
+            default: "listingimage",
+        },
+        url: {
+            type: String,
+            default:
+                "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b",
+        },
+    },
+    price : Number,
+    location : String,
+    country : String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        },
+    ],
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
