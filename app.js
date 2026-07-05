a89b6c5d2268e0c4b9c86155d7197724d77ee56d
palaksrivastava2311 <palaksri8135@gmail.com>
2026-07-05 12:06:30 +0530

    Implement sessions and flash messages

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 
 const listings = require("./routes/listing.js");
@@ -33,11 +38,47 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const sessionOptions = {
+    secret:"mysupersecretcode",
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        expires:Date.now()+7*24*60*60*1000,
+        maxAge:7*24*60*60*1000,
+        httpOnly:true,
+    },
+};
+
 app.get("/", (req,res) =>{
     res.send("Hi, I am root");
 });
 
+app.use(session(sessionOptions));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
+app.use((req,res,next)=>{
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
+
+app.get("/demouser", async(req,res)=>{
+   try{
+    let fakeUser = new User({
+        email:"student@gmail.com",
+        username:"delta-student"
+    });
+    let registeredUser = await User.register(fakeUser,"helloworld");
+    res.send(registeredUser);
+   }catch(err){
+    console.log(err);
+   }
+})
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews",reviews);
--- routes/listing.js
+++ routes/listing.js
@@ -36,6 +36,10 @@ router.get("/:id", wrapAsync(async (req, res) =>{
     let {id} = req.params;
     const listing = await Listing.findById(id).populate("reviews");
     // reviews ki id  ke saath chahte data bhi aaye issliye populate
+    if(!listing){
+        req.flash("error", "listing you requested for does not exist!");
+        return res.redirect("/listings");
+    }
     res.render("listings/show.ejs", {listing});
 }));
  
@@ -43,6 +47,7 @@ router.get("/:id", wrapAsync(async (req, res) =>{
 router.post("/",validateListing, wrapAsync(async (req,res, next)=>{
     const newListing = new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success", "New Listing Created");
     res.redirect("/listings");
 }));
 
@@ -51,6 +56,10 @@ router.post("/",validateListing, wrapAsync(async (req,res, next)=>{
 router.get("/:id/edit", wrapAsync(async (req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
+    if(!listing){
+        req.flash("error", "listing you requested for does not exist!");
+        return res.redirect("/listings");
+    }
     res.render("listings/edit.ejs", {listing});
 }));
 
@@ -58,6 +67,7 @@ router.get("/:id/edit", wrapAsync(async (req,res)=>{
 router.put("/:id", validateListing , wrapAsync(async (req,res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    req.flash("success", "Listing Updated");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -66,6 +76,7 @@ router.delete("/:id", wrapAsync(async (req,res) =>{
     let {id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success", "Listing Deleted");
     res.redirect("/listings");
 }));
 
